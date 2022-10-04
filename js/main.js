7a566df20f25f183dede6172c18bf2b0afcc2029
grechah <wkvictim@gmail.com>
2022-10-04 15:53:35 +0800

    Исправляет ошибки в задании(2.1)

--- js/main.js
+++ js/main.js
@@ -2,21 +2,18 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger
 
 function getRandomIntInclusive(min, max) {
-    if (min >= 0 && max > min) {
+  if (min >= 0 && max > min) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    return NaN;
+  }
+  return NaN;
 }
-console.log(getRandomIntInclusive(2, 7));
+getRandomIntInclusive(2, 7);
 
 // Функция для проверки максимальной длины строки
 
-function getMaxCommentLength (lineLength, maxLength) {
-    if (lineLength <= maxLength) {
-        return true;
-    }
-    return false;
+function verifyMaxCommentLength (lineLength, maxLength) {
+  return (lineLength <= maxLength);
 }
-console.log(getMaxCommentLength (25, 135));
+verifyMaxCommentLength (25, 135);
