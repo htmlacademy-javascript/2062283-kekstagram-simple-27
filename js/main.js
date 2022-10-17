55c2733136e36c2c9aceb577b42aef7ec98e6d43
grechah <wkvictim@gmail.com>
2022-10-17 15:33:56 +0800

    Выполняет задание 4.15. Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-// Функция, возвращающая случайное целое число из переданного диапазона включительно.
-// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger
+
+// Функция для проверки максимальной длины строки
+
+function verifyMaxCommentLength (lineLength, maxLength) {
+  return (lineLength <= maxLength);
+}
+verifyMaxCommentLength (25, 135);
+
+
+/* Необходимо написать функции для создания массива из 25 сгенерированных объектов.
+Каждый объект массива — описание фотографии, опубликованной пользователем.
+Объект состоит из 5 ключей:
+
+1. id, число - идентификатор опубликованной фотографии. Это число от 1 до 25. Идентификаторы не должны повторяться.
+2. url, строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} — это число от 1 до 25. Адреса картинок не должны повторяться.
+3. description, строка — описание фотографии. Описание придумайте самостоятельно.
+4. likes, число — количество лайков, поставленных фотографии. Случайное число от 15 до 200.
+5. comments, число — количество комментариев, оставленных другими пользователями к этой фотографии. Случайное число от 0 до 200.
+*/
 
 function getRandomIntInclusive(min, max) {
   if (min >= 0 && max > min) {
@@ -9,11 +26,52 @@ function getRandomIntInclusive(min, max) {
   }
   return NaN;
 }
-getRandomIntInclusive(2, 7);
 
-// Функция для проверки максимальной длины строки
-
-function verifyMaxCommentLength (lineLength, maxLength) {
-  return (lineLength <= maxLength);
+const photoLikes = [];
+for (let i = 15; i <= 200; i++) {
+  photoLikes.push(i);
 }
-verifyMaxCommentLength (25, 135);
+
+const photoComments = [];
+for (let i = 0; i <= 200; i++) {
+  photoComments.push(i);
+}
+
+const getRandomArrayElements = (element) => element
+  [getRandomIntInclusive(0, element.length - 1)];
+
+function createPhotoDescription(id) {
+  console.log();
+  return {
+    id: id,
+    likes: getRandomArrayElements(photoLikes),
+    comments: getRandomArrayElements(photoComments),
+    url: `photos/${id}.jpg`,
+    description: `Описание фотографии ${id}`
+  };
+}
+
+const restPhotos = [];
+for (let j = 0; j < 25; j++) {
+  restPhotos.push(createPhotoDescription(j));
+}
+
+
+const createPhotoDescription2 = (l1, l2) => {
+  console.log();
+  return {
+    id: l2 + 1,
+    likes: getRandomArrayElements(photoLikes),
+    comments: getRandomArrayElements(photoComments),
+    url: `photos/${l2 + 1}.jpg`,
+    description: `Описание фотографии ${l2 + 1}`
+  };
+};
+
+const restPhotos2 = Array.from({length: 25}, createPhotoDescription2);
+console.log(restPhotos2[0]);
+
+// const restPhotos3 = restPhotos2.map(createPhotoDescription2);
+// console.log(restPhotos3[0]);
+
+
