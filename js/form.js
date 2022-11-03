ac306a695082276b8342965720810510eeef2e30
grechah <wkvictim@gmail.com>
2022-11-03 19:20:04 +0800

    Выполняет задание 8.21. Правда или действие

---
+++ js/form.js
@@ -0,0 +1,27 @@
+import {isEscapeKey} from './util.js';
+const imageForm = document.querySelector('.img-upload__form')
+const uploadImage = document.querySelector('#upload-file');
+const modalForm = document.querySelector('.img-upload__overlay');
+const cancelUpload = document.querySelector('#upload-cancel');
+
+const onModalEscKeyDown = (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    modalForm.classList.add('hidden');
+  }
+};
+
+uploadImage.addEventListener('click', () => {
+  modalForm.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+
+  document.addEventListener ('keydown', (onModalEscKeyDown));
+});
+
+cancelUpload.addEventListener('click', () => {
+  modalForm.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', (onModalEscKeyDown));
+  imageForm.reset();
+});
+
