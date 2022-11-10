d03a41f9a8511b64ed30f641350a6e1ee24df2d4
grechah <wkvictim@gmail.com>
2022-11-10 19:53:03 +0800

    Выполняет задание 9

--- js/form.js
+++ js/form.js
@@ -8,11 +8,10 @@ const scaleDownButton = document.querySelector('.scale__control--smaller');
 const scaleUpButton = document.querySelector('.scale__control--bigger');
 const scaledImageContainer = document.querySelector('.img-upload__preview');
 const scaledImage = scaledImageContainer.querySelector('img');
+const effects = document.querySelectorAll('.effects__radio');
 
-console.log(scaledImage);
+
 let newValue = 100;
-
-
 
 const onModalEscKeyDown = (evt) => {
   if (evt.key === 'Escape') {
@@ -21,18 +20,19 @@ const onModalEscKeyDown = (evt) => {
   }
 };
 
-const onScaleDown = (evt) => {
+const onScaleDown = () => {
   if (newValue >= 50) {
     newValue = newValue - 25;
+    scaledImage.style.transform = `scale(${newValue / 100})`;
     scaleValue.value = `${newValue}%`;
-    scaledImage.style.transform = `scale(${newValue} / 100)`;
   }
 };
 
-const onScaleUp = (evt) => {
+const onScaleUp = () => {
   if (newValue <= 75) {
     newValue = newValue + 25;
     scaleValue.value = `${newValue}%`;
+    scaledImage.style.transform = `scale(${newValue / 100})`;
   }
 };
 
@@ -43,6 +43,13 @@ uploadImage.addEventListener('click', () => {
   document.addEventListener ('keydown', (onModalEscKeyDown));
   scaleDownButton.addEventListener('click', (onScaleDown));
   scaleUpButton.addEventListener('click', (onScaleUp));
+  scaledImage.classList.add('effects__preview--none');
+
+  for (const effect of effects) {
+    effect.onchange = () => {
+      scaledImage.className = `effects__preview--${effect.value}`;
+    };
+  }
 });
 
 cancelUpload.addEventListener('click', () => {
