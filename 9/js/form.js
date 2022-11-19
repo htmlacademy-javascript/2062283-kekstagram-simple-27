7f4f7102ac2da0f7b32c8e7541b6a943a9a60a34
Keks <keks@htmlacademy.ru>
2022-11-19 16:18:20 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/form.js
@@ -0,0 +1,97 @@
+import {successMessage} from './success.js';
+import {errorMessage} from './error.js';
+
+const imageForm = document.querySelector('.img-upload__form');
+const uploadImage = document.querySelector('#upload-file');
+const modalForm = document.querySelector('.img-upload__overlay');
+const cancelUpload = document.querySelector('#upload-cancel');
+const scaleValue = document.querySelector('.scale__control--value');
+const scaleDownButton = document.querySelector('.scale__control--smaller');
+const scaleUpButton = document.querySelector('.scale__control--bigger');
+const scaledImageContainer = document.querySelector('.img-upload__preview');
+const scaledImage = scaledImageContainer.querySelector('img');
+const effects = document.querySelectorAll('.effects__radio');
+
+
+let newValue = 100;
+
+const onModalEscKeyDown = (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    modalForm.classList.add('hidden');
+    scaledImage.className = 'effects__preview--none';
+    scaleValue.value = '100%';
+    scaledImage.style.transform = `scale(${scaleValue.value})`;
+    document.querySelector('.text__description').value = '';
+  }
+};
+
+const onScaleDown = () => {
+  if (newValue >= 50) {
+    newValue = newValue - 25;
+    scaledImage.style.transform = `scale(${newValue / 100})`;
+    scaleValue.value = `${newValue}%`;
+  }
+};
+
+const onScaleUp = () => {
+  if (newValue <= 75) {
+    newValue = newValue + 25;
+    scaleValue.value = `${newValue}%`;
+    scaledImage.style.transform = `scale(${newValue / 100})`;
+  }
+};
+
+uploadImage.addEventListener('click', () => {
+  modalForm.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  scaleValue.value = '100%';
+  document.addEventListener ('keydown', (onModalEscKeyDown));
+  scaleDownButton.addEventListener('click', (onScaleDown));
+  scaleUpButton.addEventListener('click', (onScaleUp));
+  scaledImage.classList.add('effects__preview--none');
+
+  for (const effect of effects) {
+    effect.onchange = () => {
+      scaledImage.className = `effects__preview--${effect.value}`;
+    };
+  }
+});
+
+imageForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  const formData = new FormData(evt.target);
+
+  fetch(
+    'https://27.javascript.pages.academy/kekstagram-simple',
+    {
+      method: 'POST',
+      body: formData,
+    },
+  )
+    .then((response) => {
+      if (response.ok) {
+        scaledImage.className = 'effects__preview--none';
+        scaleValue.value = '100%';
+        scaledImage.style.transform = `scale(${scaleValue.value})`;
+        evt.target.reset();
+        modalForm.classList.add('hidden');
+        successMessage.classList.remove('hidden');
+
+      } else {
+        errorMessage.classList.remove('hidden');
+      }
+    });
+});
+
+cancelUpload.addEventListener('click', () => {
+  scaledImage.className = 'effects__preview--none';
+  scaleValue.value = '100%';
+  scaledImage.style.transform = `scale(${scaleValue.value})`;
+  modalForm.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', (onModalEscKeyDown));
+  this.form.reset();
+});
+
+
---
+++ 9/js/pictures.js
@@ -0,0 +1,31 @@
+
+const picturesContainer = document.querySelector('.pictures');
+
+const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const fragment = document.createDocumentFragment();
+
+const renderUserPics = (data) => {
+
+  for (let i = 0; i < data.length; i++) {
+    const thumbnailElement = thumbnailTemplate.cloneNode(true);
+
+    const elementLink = thumbnailElement.querySelector('.picture__img');
+    elementLink.src = data[i].url;
+
+    const elementLikes = thumbnailElement.querySelector('.picture__likes');
+    elementLikes.textContent = data[i].likes;
+
+    const elementComments = thumbnailElement.querySelector('.picture__comments');
+    elementComments.textContent = data[i].comments;
+
+
+    fragment.appendChild(thumbnailElement);
+  }
+
+  picturesContainer.appendChild(fragment);
+
+};
+
+export {renderUserPics};
+
